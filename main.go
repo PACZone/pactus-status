5126147901a371feeb125e5d8f480e6b40b37660
Kay <kehiiiiya@gmail.com>
2024-03-08 10:25:39 +0000

    feat: totoal pac exist

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 func PostUpdates(ctx context.Context, b *bot.Bot, cmgr *client.Mgr) {
 	m, _ := b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: "@pactus_status",
-		Text: ".",
+		Text:   ".",
 	})
 
 	messageID := m.ID
@@ -92,6 +92,7 @@ func makeMessage(b *pactus.GetBlockchainInfoResponse, c, timeDiff int64, status,
 	s.WriteString(fmt.Sprintf("🦾 **%v** PAC Staked\n\n", formatNumber(int64(util.ChangeToCoin(b.TotalPower)))))
 	s.WriteString(fmt.Sprintf("🦾 **%v PAC** Committee Power\n\n", formatNumber(int64(util.ChangeToCoin(b.CommitteePower)))))
 	s.WriteString(fmt.Sprintf("🔄 **%v PAC** Circulating Supply\n\n", formatNumber(int64(util.ChangeToCoin(c)))))
+	s.WriteString(fmt.Sprintf("🪙 **%v PAC** Total exist\n\n", formatNumber(int64(util.ChangeToCoin(c+b.TotalPower)))))
 
 	s.WriteString(fmt.Sprintf("```🧑🏻‍⚕️NetworkStatus Network is %s\n\n%s is The LastBlock time and there is %v seconds passed from last block```", status, lastBlkTime, timeDiff))
 
