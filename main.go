0f7b728d9f3ad54600407ffde4a0aaf3d79d07c7
Kay <kehiiiiya@gmail.com>
2024-03-09 21:39:47 +0000

    feat: adding the price

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func makeMessage(b *pactus.GetBlockchainInfoResponse, c, timeDiff int64, status,
 	s.WriteString(fmt.Sprintf("🪙 %v Total PAC Exist\n\n", formatNumber(int64(util.ChangeToCoin(c+b.TotalPower)))))
 
 	s.WriteString("Note This the last price of Exbitron and it's an unofficial listing\nno financial advice/DYOR\n")
-	s.WriteString(fmt.Sprintf("📈 Exbitron Price \\%s$\\ \n\n", price))
+	s.WriteString(fmt.Sprintf("📈 Exbitron Price \\%s\\$ \n\n", price))
 
 	s.WriteString(fmt.Sprintf("```🧑🏻‍⚕️NetworkStatus Network is %s\n\n%s is The LastBlock time and there is %v seconds passed from last block```", status, lastBlkTime, timeDiff))
 
